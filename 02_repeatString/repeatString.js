2a57eaa8cb944d9cc6b8d1f111f0bb1161391c67
Orkhyd <marius.roelly@gmail.com>
2023-02-14 17:48:40 +0100

    First and Second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, repeatNb) {
+  let finalStr = "";
+  for (let i = 0; i < repeatNb; i++) {
+    finalStr += str;
+  }
+  if (repeatNb >= 0) {
+    return finalStr;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
