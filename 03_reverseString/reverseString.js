1b645513a756e08586b56fffa36eddad400935d7
Orkhyd <marius.roelly@gmail.com>
2023-02-15 12:00:18 +0100

    ex 3 - 6

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (initWord) {
+  let retWord = "";
+  for (let i = 0; i < initWord.length; i++) {
+    retWord += initWord[initWord.length - i - 1];
+  }
+  return retWord;
 };
 
 // Do not edit below this line
